8f1d014c20c1453c8eebb005ee908fd9a55c8f24
Adrián Burgos Gálvez <adrianburgosgalvez@gmail.com>
2023-09-20 22:48:07 +0200

    Version01

---
+++ Prueba3.js
@@ -0,0 +1,11 @@
+// Ejercicio3
+
+let valor;
+
+switch (prompt("Dime un número: ")) {
+    case 1: valor = "uno";
+    case 2: valor = "dos";
+    case 3: valor = "tres";
+}
+
+console.log(valor);
---
+++ Prueba5.js
@@ -0,0 +1,3 @@
+//Ejercicio5
+
+frutas:
